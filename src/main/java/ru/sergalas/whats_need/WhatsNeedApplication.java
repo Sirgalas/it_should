36a258aa5e52e3510a9a2b2f71a4c0a81b1e32e2
Sergalas <svbeloventcev@sberinsur.ru>
2026-05-18 16:34:03 +0300

    first commit

---
+++ src/main/java/ru/sergalas/whats_need/WhatsNeedApplication.java
@@ -0,0 +1,13 @@
+package ru.sergalas.whats_need;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsNeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsNeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sergalas/whats_need/WhatsNeedApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergalas.whats_need;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsNeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
